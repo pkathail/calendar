c80b1431c8bc810584775695989f131871691a3f
Alan Du <alanhdu@gmail.com>
2015-04-10 21:18:39 -0400

    Add tests
    fixup

--- cal/__init__.py
+++ cal/__init__.py
@@ -77,7 +77,7 @@ def home():
 def events():
     now = dt.datetime.now()
     events = Event.query.filter(Event.start > now) \
-        .filter(Event.start < now + dt.timedelta(weeks=1))
+                        .filter(Event.start < now + dt.timedelta(weeks=1))
 
     return jsonify(data=[event.to_json() for event in events.all()])
 
@@ -88,9 +88,9 @@ def users():
     events = Event.query.filter(Event.start > now) \
                         .filter(Event.start < now + dt.timedelta(weeks=1))
 
-    users = {event.user for event in events}    # use set to make users unique
-    users = [user.to_json() for user in sorted(users, key=lambda u: u.name)]
-    return jsonify(data=users)
+    # use set to make users unique
+    users = sorted({event.user for event in events}, key=lambda u: u.name)
+    return jsonify(data=[user.to_json() for user in users])
 
 
 @app.route("/search/<searchfield>")
